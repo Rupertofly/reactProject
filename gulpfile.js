8f50c4cb04ad9c89e8f453355399d4cfd2f0b91f
Ruby Quail <ruby@rubyquail.design>
2020-02-04 16:57:41 +1100

    ok just before I start working on webpack
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const gc = require('gulp-changed');
 const ts = require('gulp-typescript');
 const sm = require('gulp-sourcemaps');
 const rm = require('rimraf');
+const nm = require('gulp-nodemon');
 gulp.task('server:clean', cb => {
   rm('./dist', () => cb());
 });
@@ -22,11 +23,24 @@ gulp.task(
       .pipe(gulp.dest('./dist'));
   })
 );
+function watchServer() {
+  return gulp
+    .watch('./src/server/**/*.ts', gulp.series('server:build'))
+    .on('error', () => {});
+}
 gulp.task(
   'server:watch',
-  gulp.series('server:build', function watch() {
-    return gulp
-      .watch('./src/server/**/*.ts', gulp.series('server:build'))
-      .on('error', () => {});
-  })
+  gulp.series('server:build', watchServer)
+);
+gulp.task(
+  'server:dev',
+  gulp.series(
+    'server:build',
+    gulp.parallel(watchServer, function nodemonTask() {
+      return nm({
+        script: './server.js',
+        watch: 'dist',
+      });
+    })
+  )
 );
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,35 @@
-const abs = { a: 'server' };
-const cc = { ...abs, kyle: 'james' }
-console.table( [`This is a Table`, `it is on my ${cc.a}`] );
-throw new Error( 'wil it work' );
+import * as express from 'express';
+import * as http from 'http';
+import { isDevelopment } from './settings';
+
+/* ---------------------------------- */
+/* SETUP                              */
+/* ---------------------------------- */
+
+const app = express();
+const server = new http.Server( app );
+
+/* config --------------------------- */
+
+app.set( 'view engine', 'pug' )
+app.use( express.static( "public" ) );
+const useExternalStyles = !isDevelopment;
+const scriptRoot = isDevelopment ? 'http://localhost:8080' : '/build'
+app.get( '*', ( req, res ) => {
+
+  res.render( 'index', {
+    useExternalStyles, scriptRoot
+  } )
+
+} )
+
+/* ---------------------------------- */
+/* STARTUP                            */
+/* ---------------------------------- */
+
+const port = process.env.PORT ?? 3000;
+server.listen( port, () => {
+
+  console.log( `started http server on ${port}` );
+
+} )

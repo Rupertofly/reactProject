5321f2eb4b4e36f2313777bcac9e7e5caf34ebbf
Ruby Quail <ruby@rubyquail.design>
2020-02-05 16:55:25 +1100

    ok getting that work done
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,26 @@
-"use strict";
-exports.__esModule = true;
-var path = require("path");
-var _ = require("lodash");
-var webpack = require("webpack");
-var vendor = ['lodash'];
-function createConfig(isDebug) {
-    var devTool = isDebug ? 'cheap-module-source-map' : null;
-    var plugins = [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: "\"" + (process.env.NODE_ENV || 'development') + "\""
-            },
-            IS_PRODUCTION: !isDebug,
-            IS_DEVELOPMENT: isDebug
-        }),
-    ];
-    var loaders = {
-        ts: {
-            test: /\.tsx?/,
-            loader: 'ts-loader',
-            exclude: /node_modules/
-        },
-        eslint: {
-            test: /\.tsx?/,
-            loader: 'eslint-loader',
-            exclude: /node_modules/
-        },
-        json: {
-            test: /\.json$/,
-            loader: 'json-loader'
-        },
-        css: { test: /\.css$/, loader: 'style-loader!css-loader?sourceMap' },
-        sass: {
-            test: /.scss$/,
-            loader: 'style-loader!css-loader?sourceMap!sass-loader?sourceMap'
-        },
-        files: {
-            test: /\.(png|jpg|jpeg|gif|woff|ttf|eot|svg|woff2)/,
-            loader: 'url-loader?limit=5000'
-        }
-    };
-    var clientEntry = ['./src/client/client.ts'];
-    var publicPath = '/dist/';
-    if (isDebug) {
-    }
-    else {
-    }
-    return {
-        mode: isDebug ? 'development' : 'production',
-        name: 'client',
-        devtool: devTool || undefined,
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    vendor: {
-                        test: /node_modules/,
-                        chunks: 'initial',
-                        name: 'vendor',
-                        enforce: true
-                    }
-                }
-            }
-        },
-        entry: {
-            app: clientEntry,
-            vendor: vendor
-        },
-        output: {
-            path: path.join(__dirname, 'public', 'dist'),
-            filename: '[name].js',
-            publicPath: publicPath
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx'],
-            alias: { shared: path.join(__dirname, 'src', 'server', 'shared') }
-        },
-        module: {
-            rules: _.values(loaders)
-        },
-        plugins: plugins
-    };
-}
-exports.createConfig = createConfig;
-exports["default"] = createConfig(process.env.NODE_ENV !== 'production');
+/* eslint-disable */
+
+const webpack = require('webpack');
+const path = require('path');
+const mec = require('mini-css-extract-plugin');
+
+/** @type {webpack.Configuration} */
+const config = {
+  entry: './src/client/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'main.js',
+    mode: process.env.NODE_ENV,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use:
+          process.env.NODE_ENV !== 'production'
+            ? ['style-loader', 'css-loader', 'sass-loader']
+            : [mec.loader, 'style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
